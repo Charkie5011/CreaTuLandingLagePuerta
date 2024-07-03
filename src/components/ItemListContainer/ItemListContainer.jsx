4ff285c5caf206a071bc6451ab3815241f111274
Charkie5011 <clagepuerta@gmail.com>
2024-07-02 22:57:01 -0300

    loader implementado

--- src/components/ItemListContainer/ItemListContainer.jsx
+++ src/components/ItemListContainer/ItemListContainer.jsx
@@ -3,26 +3,34 @@ import { getProducts, getProductsByBrand } from "../../data/asyncMock";
 import ItemList from "../ItemList/ItemList";
 import FilterContainer from "../FilterContainer/FilterContainer";
 import { useParams } from "react-router-dom";
-
+import { RotateLoader } from "react-spinners";
+import style from "./ItemListContainer.module.css"
 
 const ItemListContainer = () => {
   const [productos, setProductos] = useState([]);
-  const {brandId} = useParams();
-  console.log(brandId)
-  useEffect(
-    () =>{
-      const data = brandId ? getProductsByBrand(brandId) : getProducts()
+  const { brandId } = useParams();
+  const [loading, setLoading] = useState(true);
+  console.log(brandId);
+  useEffect(() => {
+    setLoading(true);
+    const data = brandId ? getProductsByBrand(brandId) : getProducts();
 
-      data
-        .then((productos) => setProductos(productos))
-        .catch((error) => console.log(error))},
-    [brandId]
+    data
+      .then((productos) => setProductos(productos))
+      .catch((error) => console.log(error))
+      .finally(() => setLoading(false));
+  }, [brandId]);
+  return (
+    <div className={style.itemListContainer}>
+      <h3>Showroom</h3>
+      <FilterContainer />
+      {loading ? (
+        <RotateLoader />
+      ) : (
+        <ItemList className="shopDisplay" stock={productos} />
+      )}
+    </div>
   );
-  return <div>
-    <h3>Showroom</h3>
-    <FilterContainer/>
-    <ItemList className="shopDisplay" stock={productos}/>
-  </div>;
 };
 
 export default ItemListContainer;
